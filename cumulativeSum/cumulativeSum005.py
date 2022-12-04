c9dc25f2d3002c8c3d6b52faa8b73335ffee09a1
Stead08 <62312581+Stead08@users.noreply.github.com>
2022-12-04 14:58:31 +0900

    Create cumulativeSum005.py
    正方行列の場合

---
+++ cumulativeSum/cumulativeSum005.py
@@ -0,0 +1,17 @@
+#行列の行数n
+#正方行列の場合を仮定
+n = int(input())
+#求めたい領域の左上(a, b),右下(c, d)
+a, b, c, d = map(int, input().split())
+l = []
+for _ in range(n):
+    l.append(list(map(int, input().split())))
+
+#累積和の行列を作成
+s = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(n):
+    for j in range(n):
+        s[i + 1][j + 1] = s[i + 1][j] + s[i][j + 1] - s[i][j] + l[i][j]
+
+print(s[c + 1][d + 1] - s[a][d + 1] - s[c + 1][b] + s[a][b])

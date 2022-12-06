7b46c0edb656a4c726925137f1aaa29b6d73232e
Stead08 <62312581+Stead08@users.noreply.github.com>
2022-12-06 18:43:20 +0900

    Create binary_search__application_step1.py

---
+++ binarySearch/binary_search__application_step1.py
@@ -0,0 +1,20 @@
+#https://paiza.jp/works/mondai/reviews/show/ca9d987d8f775f45855f98ef7524377d
+n, k = map(int, input().split())
+W = [int(x) for x in input().split()]
+V = [int(x) for x in input().split()]
+
+left, right = 0, 5001
+for _ in range(100):
+    mid = (left + right) / 2
+
+    tmp = [0] * n
+    for i in range(n):
+        tmp[i] = V[i] - W[i] * mid
+    tmp.sort(reverse=True)
+
+    if sum(tmp[:k]) >= 0:
+        left = mid
+    else:
+        right = mid
+
+print(left)

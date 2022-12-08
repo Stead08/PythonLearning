e43de43132de7490d6c8888717ed13bec373c829
Stead08 <62312581+Stead08@users.noreply.github.com>
2022-12-08 23:18:11 +0900

    Update move_threetimes.py
    コメント追加中

--- bfs_dfs/move_threetimes.py
+++ bfs_dfs/move_threetimes.py
@@ -6,18 +6,18 @@ H, W = map(int, input().split())
 #開始位置
 y, x = map(int, input().split())
 
-q = deque()
-q.append((y, x))
+q = deque()　#dequeオブジェクトの作成
+q.append((y, x))　#queに初期位置を入れる
 l = [[-1] * W for _ in range(H)]
 l[y][x] = 0
 mp = [["."] * W for _ in range(H)]
 mp[y][x] = "*"
 
 while q:
-    ny, nx = q.popleft()
-    if l[ny][nx] == 3:
-        continue
-    for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+    ny, nx = q.popleft()　#queの先頭要素を取り出してqueから削除 pop-left
+    if l[ny][nx] == 3: #三回移動済みだったら
+        continue　
+    for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1)):　#今いる位置から上下左右の位置について
         if 0 <= ny + dy < H and 0 <= nx + dx < W:
             if l[ny + dy][nx + dx] == -1:
                 l[ny + dy][nx + dx] = l[ny][nx] + 1

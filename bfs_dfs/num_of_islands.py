75253793294409b36619555d51dadcc5693c0d29
Stead08 <62312581+Stead08@users.noreply.github.com>
2023-01-07 21:12:18 +0900

    Create num_of_islands.py

---
+++ bfs_dfs/num_of_islands.py
@@ -0,0 +1,20 @@
+def dfs(y: int, x: int):
+    S[y][x] = "#"
+    for i in range(-1, 2, 2):
+        if 0 <= y + i < H and S[y + i][x] == ".":
+            dfs(y + i, x)
+        if 0 <= x + i < W and S[y][x + i] == ".":
+            dfs(y, x + i)
+
+
+H, W = map(int, input().split())
+S = [list(input()) for _ in range(H)]
+
+ans = 0
+for i in range(H):
+    for j in range(W):
+        if S[i][j] == ".":
+            dfs(i, j)
+            ans += 1
+
+print(ans)

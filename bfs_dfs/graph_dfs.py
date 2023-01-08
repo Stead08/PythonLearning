b4e019265ae535ecf96c9c54d14be9e8e600a4de
Stead08 <62312581+Stead08@users.noreply.github.com>
2023-01-08 11:08:58 +0900

    Update graph_dfs.py

--- bfs_dfs/graph_dfs.py
+++ bfs_dfs/graph_dfs.py
@@ -2,25 +2,28 @@
 import sys
 sys.setrecursionlimit(10 ** 6)
 #頂点の数 N と辺の本数 M, 頂点番号 X 
-n, m, x = map(int, input().split())
-g = [[] for _ in range(n)]
-for _ in range(n-1):
+
+def dfs(now: int):
+    unvisited[now] = False
+    print(now + 1)
+    for i in range(len(graph[now])):
+        if unvisited[graph[now][i]]:
+            dfs(graph[now][i])
+
+
+N, M, X = map(int, input().split())
+X -= 1
+
+graph = [[] for _ in range(N)]
+for _ in range(M):
     a, b = map(int, input().split())
     a -= 1
     b -= 1
-    g[a].append(b)
-    g[b].append(a)
+    graph[a].append(b)
+    graph[b].append(a)
 
-for i in range(n):
-    g[i].sort()
-visited = [False] * n    
+for i in range(N):
+    graph[i].sort()
 
-x -= 1
-
-def dfs(x):
-    visited[x] = True
-    print(x+1)
-    for i in range(len(g[x])):
-        if visited[g[x][i]] == False:
-            dfs(g[x][i])
-dfs(x)
+unvisited = [True] * N
+dfs(X)
